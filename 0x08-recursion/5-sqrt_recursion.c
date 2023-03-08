61d36a458b1b6a7331e743c78793f6cc98593299
root <michellemigot125@gmail.com>
2023-03-07 23:35:49 -0800

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 int _sqrt(int prev, int root);
 
 /**
@@ -12,4 +12,19 @@ int _sqrt_recursion(int n)
 		return (-1);
 
 	return (_sqrt(1, n));
+}
+
+/**
+ * _sqrt - find square root
+ * @prev: previous value
+ * @root: square root value
+ * Return: the square root
+ */
+int _sqrt(int prev, int root)
+{
+	if (prev > root)
+		return (-1);
+	else if (prev * prev == root)
+		return (prev);
+	return (_sqrt(prev + 1, root));
 }
